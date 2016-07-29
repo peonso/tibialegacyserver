f3edf8f4fb3940caa0d04b5be00a85e066954191
peonso <peonso@hotmail.com>
2016-07-29 19:26:27 -0400

    Minor storages changes

--- server/data/creaturescripts/scripts/record.lua
+++ server/data/creaturescripts/scripts/record.lua
@@ -1,5 +1,4 @@
 function onLogin(cid)
-	local PlayersRecordStorage = 14301
 	local i = 1
 	
 	for _, pid in ipairs(getPlayersOnlineList()) do
@@ -7,15 +6,15 @@ function onLogin(cid)
 	end
 	local current = i - 1
 	
-	if getEternalStorageValue(PlayersRecordStorage) == nil then
-		setEternalStorageValue(PlayersRecordStorage, 0)
+	if getEternalStorageValue(STORAGE_SERVERRECORD) == nil then
+		setEternalStorageValue(STORAGE_SERVERRECORD, 0)
 	end	
 	
-	oldRecord = getEternalStorageValue(PlayersRecordStorage)
+	oldRecord = getEternalStorageValue(STORAGE_SERVERRECORD)
 	
 	if current > oldRecord then
 		addEvent(doBroadcastMessage, 150, "New record: " .. current .. " players are logged in.", MESSAGE_STATUS_DEFAULT)
-		setEternalStorageValue(PlayersRecordStorage, current)
+		setEternalStorageValue(STORAGE_SERVERRECORD, current)
 		db.executeQuery("INSERT INTO `server_record` (`record`, `timestamp`) VALUES (" .. current .. ", " .. os.time() .. ");")
 		if current >= 2 then
 			print(" - New record. " .. current .. " players. " ..os.date("%d %b %H:%M", (os.time())))
