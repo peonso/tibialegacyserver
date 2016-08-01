3d7ec2530075162b5d2421a41c2094ff7084033f
peonso <peonso@hotmail.com>
2016-07-31 20:44:34 -0400

    Update spells formulas
    spell formulas updated to make them closer to 7.72 correct values as
    stated here
    https://docs.google.com/spreadsheets/d/1293KCxxpovzlJk6QAnrZ52Hgr9LA_H5YlMPhgEhGS8s/edit?usp=sharing

--- server/data/spells/scripts/monster/paralyze_carniphila.lua
+++ server/data/spells/scripts/monster/paralyze_carniphila.lua
@@ -1,6 +1,7 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, CONST_ME_MAGIC_GREEN)
-setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_RED)
+setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_POISONAREA)
+setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_POISON)
 
 local condition = createConditionObject(CONDITION_PARALYZE)
 setConditionParam(condition, CONDITION_PARAM_TICKS, 30*1000)
