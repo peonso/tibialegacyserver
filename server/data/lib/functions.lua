9cf1afb9f09c9efe2d6187bdb808d1fe620d1009
peonso <peonso@hotmail.com>
2016-09-07 12:18:52 -0400

    Small fixes.
    Updated some RME files
    Fixed some functions
    Fixed general murious corpse id

--- server/data/lib/functions.lua
+++ server/data/lib/functions.lua
@@ -915,8 +915,12 @@ function doPlayerBuyItemContainer(cid, containerid, itemid, count, cost, charges
 	return doPlayerRemoveMoney(cid, cost) and doPlayerGiveItemContainer(cid, containerid, itemid, count, charges)
 end
 
-function getArticle(str)
+function getItemArticleById(id)
 	return getItemDescriptions(id).article
+end
+
+function getArticleByWord(str)
+	return str:find("[AaEeIiOoUuYy]") == 1 and "an" or "a"
 end
 
 function getItemNameById(itemid)
