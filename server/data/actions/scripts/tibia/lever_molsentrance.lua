a68d211ce95447f31b3887799cc6cf87d76ae47d
peonso <peonso@hotmail.com>
2016-09-21 17:17:59 -0400

    minor fixes
    fix mols entrance script
    fix items.xml liquids
    update Pemaret npc script
    update create item talkaction

--- server/data/actions/scripts/tibia/lever_molsentrance.lua
+++ server/data/actions/scripts/tibia/lever_molsentrance.lua
@@ -1,6 +1,6 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local gatePos = {x=32483, y=31633, z=9, stackpos=0}
-	local nextTile = {x=gatePos.x, y=gatePos.y+1, z=gatePos.z}
+	local nextTile = {x=gatePos.x, y=gatePos.y, z=gatePos.z+1}
 	local gateItem = getThingfromPos(gatePos)
 
 	if item.itemid == 1945 and (gateItem.itemid == 355 or gateItem.itemid == 354) then
