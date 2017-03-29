bcfe9b1cd48343404468cfe4bf499e45a867676f
tarantonio <tarantonio@users.noreply.github.com>
2017-03-29 09:08:21 +0200

    Fix Withdraw

--- server/data/npc/scripts/Lokur.lua
+++ server/data/npc/scripts/Lokur.lua
@@ -212,7 +212,7 @@ function creatureSayCallback(cid, type, msg)
 	elseif	msgcontains(msg, 'withdraw all') then
 		WITHDRAWMONEY = getPlayerBalance(cid)
 		if WITHDRAWMONEY >= 1 then
-			if getPlayerBalance(cid) > WITHDRAWMONEY then
+			if getPlayerBalance(cid) >= WITHDRAWMONEY then
 				npcHandler:say("Would you like to withdraw ".. WITHDRAWMONEY .." gold?", 1)
 				talk_state = 303
 			else
@@ -227,7 +227,7 @@ function creatureSayCallback(cid, type, msg)
 	elseif msgcontains(msg, 'withdraw') then
 		WITHDRAWMONEY = getMoneyCount(msg)
 		if WITHDRAWMONEY >= 1 then
-			if getPlayerBalance(cid) > WITHDRAWMONEY then
+			if getPlayerBalance(cid) >= WITHDRAWMONEY then
 				npcHandler:say("Would you like to withdraw ".. WITHDRAWMONEY .." gold?", 1)
 				talk_state = 303
 			else
@@ -242,7 +242,7 @@ function creatureSayCallback(cid, type, msg)
 	elseif talk_state == 301 then
 		WITHDRAWMONEY = getMoneyCount(msg)
 		if WITHDRAWMONEY >= 1 then
-			if getPlayerBalance(cid) > WITHDRAWMONEY then
+			if getPlayerBalance(cid) >= WITHDRAWMONEY then
 				npcHandler:say("Would you like to withdraw ".. WITHDRAWMONEY .." gold?", 1)
 				talk_state = 303
 			else
