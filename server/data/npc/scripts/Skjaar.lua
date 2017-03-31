3782ccb80384160e86b98dfa174a5ff1147a3f97
tarantonio <tarantonio@users.noreply.github.com>
2017-03-31 12:56:08 +0200

    Noble armor quest Skjaar update

--- server/data/npc/scripts/Skjaar.lua
+++ server/data/npc/scripts/Skjaar.lua
@@ -7,25 +7,27 @@ NpcSystem.parseParameters(npcHandler)
 
 
 -- OTServ event handling functions
-function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
-function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
+function onCreatureAppear(cid)				npcHandler:onCreatureAppear(cid)			end
+function onCreatureDisappear(cid)			npcHandler:onCreatureDisappear(cid)			end
 function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
-function onThink()				npcHandler:onThink()					end
+function onThink()							npcHandler:onThink()					end
 
 function greetCallback(cid)
-	if getPlayerLevel(cid) <= 14 then
-	npcHandler:setMessage(MESSAGE_GREET, "I don't talk to little children!!")
-	return false
-	
-	elseif getPlayerLevel(cid) >= 15 then
+	if getPlayerLevel(cid) < 15 then
+		--npcHandler:setMessage(MESSAGE_GREET, "I don't talk to little children!!")
+		npcHandler:say("I don't talk to little children!!", 1)
+		npcHandler:releaseFocus()
+		npcHandler:resetNpc()
+		return false		
+	else
 		if getPlayerVocation(cid) == 1 or getPlayerVocation(cid) == 5 then
-		npcHandler:setMessage(MESSAGE_GREET, "It's good to see somebody who has chosen the path of wisdom. What do you want?")
+			npcHandler:setMessage(MESSAGE_GREET, "It's good to see somebody who has chosen the path of wisdom. What do you want?")
 		elseif getPlayerVocation(cid) == 2 or getPlayerVocation(cid) == 6 then
-		npcHandler:setMessage(MESSAGE_GREET, "Hail, friend of nature! How may I help you?")
+			npcHandler:setMessage(MESSAGE_GREET, "Hail, friend of nature! How may I help you?")
 		elseif getPlayerVocation(cid) == 3 or getPlayerVocation(cid) == 7 then
-		npcHandler:setMessage(MESSAGE_GREET, "Neither strong enough to be a knight nor wise enough to be a real mage. You like it easy, don't you? Why are you disturbing me?")
+			npcHandler:setMessage(MESSAGE_GREET, "Neither strong enough to be a knight nor wise enough to be a real mage. You like it easy, don't you? Why are you disturbing me?")
 		elseif getPlayerVocation(cid) == 4 or getPlayerVocation(cid) == 8 then
-		npcHandler:setMessage(MESSAGE_GREET, "Another creature who believes thinks physical strength is more important than wisdom! Why are you disturbing me?")
+			npcHandler:setMessage(MESSAGE_GREET, "Another creature who believes thinks physical strength is more important than wisdom! Why are you disturbing me?")
 		end
 		return true
 	end	
@@ -47,6 +49,119 @@ keywordHandler:addKeyword({'master'}, StdModule.say, {npcHandler = npcHandler, o
 function creatureSayCallback(cid, type, msg) msg = string.lower(msg)
 	if(npcHandler.focus ~= cid) then
 		return false
+	end
+	if getPlayerLevel(cid) <= 14 and msgcontains(msg, '') then
+	npcHandler:say("Get lost you little freak!", 1)
+	npcHandler:releaseFocus()
+	npcHandler:resetNpc()	
+	end
+	
+if msgcontains(msg, 'key') or msgcontains(msg, 'Key') then
+	npcHandler:say("I will give the key to the crypt only to the closest followers of my master. Would you like me to test you?", 1)
+	talk_state = 1
+
+elseif talk_state == 1 and msgcontains(msg, 'yes') or talk_state == 1 and msgcontains(msg, 'Yes') then
+	npcHandler:say("Before we start I must ask you for a small donation of 1000 gold coins. Are you willing to pay 1000 gold coins for the test?", 1)
+	talk_state = 2
+elseif talk_state == 1 and msgcontains(msg, 'no') then
+	npcHandler:say("Then leave, unworthy worm!", 1)
+	talk_state = 0	
+elseif talk_state == 1 and msgcontains(msg, '') then
+	npcHandler:say("You're not worthy if you cannot make up your mind. Leave!", 1)
+	talk_state = 0
+	npcHandler:releaseFocus()
+	npcHandler:resetNpc()	
+	
+elseif talk_state == 2 and msgcontains(msg, 'yes') or talk_state == 2 and msgcontains(msg, 'Yes') then
+	if doPlayerRemoveMoney(cid, 1000) == true then
+	npcHandler:say("All right then. Here comes the first question. What was the name of Dago's favourite pet?", 1)
+	talk_state = 3		
+	else
+	npcHandler:say("You don't even have the money to make a donation? Then go!", 1)
+	talk_state = 0
+	npcHandler:releaseFocus()
+	npcHandler:resetNpc()		
+	end
+elseif talk_state == 2 and msgcontains(msg, 'no') then
+	npcHandler:say("Then leave, unworthy worm!", 1)
+	talk_state = 0	
+elseif talk_state == 2 and msgcontains(msg, '') then
+	npcHandler:say("You're not worthy if you cannot make up your mind. Leave!", 1)
+	talk_state = 0
+	npcHandler:releaseFocus()
+	npcHandler:resetNpc()	
+	
+elseif talk_state == 3 and msgcontains(msg, 'redips') or talk_state == 2 and msgcontains(msg, 'Redips') then
+	npcHandler:say("Perhaps you knew him after all. Tell me - how many fingers did he have when he died?", 1)
+	talk_state = 4
+elseif talk_state == 3 and msgcontains(msg, '') then
+	npcHandler:say("You are wrong. Get lost!", 1)
+	talk_state = 0
+	npcHandler:releaseFocus()
+	npcHandler:resetNpc()
+	
+elseif talk_state == 4 and msgcontains(msg, '7') or talk_state == 4 and msgcontains(msg, 'seven') or talk_state == 4 and msgcontains(msg, 'Seven') then
+	npcHandler:say("Also true. But can you also tell me the colour of the deamons in which master specialized?", 1)
+	talk_state = 5
+elseif talk_state == 4 and msgcontains(msg, '') then
+	npcHandler:say("You are wrong. Get lost!", 1)
+	talk_state = 0
+	npcHandler:releaseFocus()
+	npcHandler:resetNpc()	
+	
+elseif talk_state == 5 and msgcontains(msg, 'black') or talk_state == 5 and msgcontains(msg, 'Black') then
+	npcHandler:say("It seems you are worthy after all. Do you want the key to the crypt?", 1)
+	talk_state = 6
+elseif talk_state == 5 and msgcontains(msg, '') then
+	npcHandler:say("You are wrong. Get lost!", 1)
+	talk_state = 0
+	npcHandler:releaseFocus()
+	npcHandler:resetNpc()	
+	
+elseif talk_state == 6 and msgcontains(msg, 'yes') or talk_state == 6 and msgcontains(msg, 'Yes') then
+	NOBLEARMORKEY = doPlayerAddItem(cid, 2089, 1)
+	doSetItemActionId(NOBLEARMORKEY, 3142)
+	doSetItemSpecialDescription(NOBLEARMORKEY, "(Key: 3142)")
+	npcHandler:say("Here you are.", 1)
+	talk_state = 0
+	
+elseif talk_state == 6 and msgcontains(msg, '') then
+	npcHandler:say("It is always a wise decision to leave the dead alone.", 1)
+	talk_state = 0	
+	
+elseif msgcontains(msg, "idiot") then
+	doSendMagicEffect(getCreaturePosition(getNpcCid(  )), 13)
+	doSendMagicEffect(getPlayerPosition(cid), 15)
+	doCreatureAddHealth(cid, -getCreatureHealth(cid) +1)
+	npcHandler:say("Take this for your words!", 1)
+	npcHandler:releaseFocus()
+	npcHandler:resetNpc()
+	talk_state = 0
+	
+elseif msgcontains(msg, "asshole") then
+	doSendMagicEffect(getCreaturePosition(getNpcCid(  )), 13)
+	doSendMagicEffect(getPlayerPosition(cid), 15)
+	doCreatureAddHealth(cid, -getCreatureHealth(cid) +1)
+	npcHandler:say("Take this for your words!", 1)
+	npcHandler:releaseFocus()
+	npcHandler:resetNpc()
+	talk_state = 0
+	
+elseif msgcontains(msg, "fuck") then
+	doSendMagicEffect(getCreaturePosition(getNpcCid(  )), 13)
+	doSendMagicEffect(getPlayerPosition(cid), 15)
+	doCreatureAddHealth(cid, -getCreatureHealth(cid) +1)
+	npcHandler:say("Take this for your words!", 1)
+	npcHandler:releaseFocus()
+	npcHandler:resetNpc()
+	talk_state = 0
+	
+end		
+    return true
+end
+
+npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
+npcHandler:addModule(FocusModule:new())
 	end
 	if getPlayerLevel(cid) <= 14 and msgcontains(msg, '') then
 	npcHandler:say("Get lost you little freak!", 1)
